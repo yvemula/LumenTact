9c5d14add8ca068a646363727f68c26131250156
Joseph Pan <108947845+Enigma-iwnl@users.noreply.github.com>
2025-10-18 00:54:13 -0400

    detect change

--- detect.py
+++ detect.py
@@ -1,6 +1,8 @@
 import argparse
 from ultralytics import YOLO
 
+from .haptics import generate_haptic_feedback
+
 
 def main():
     ap = argparse.ArgumentParser()
@@ -9,7 +11,16 @@ def main():
 
     model = YOLO("yolov8n.pt")
 
-    results = model(args.input, show=True, save=True)
+    results_generator = model(args.input, stream=True, show=True, save=True)
+
+    for r in results_generator:
+
+        if r.boxes:
+            for box in r.boxes:
+                cls_id = int(box.cls)
+                class_name = model.names[cls_id]
+
+                generate_haptic_feedback(class_name)
 
 
 if __name__ == "__main__":
